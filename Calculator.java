cbc8f2019aed7c224705cd59c25ebab10f182a85
shailusri14 <shailusripendota@gmail.com>
2026-02-19 09:51:33 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,7 @@ class Calculator
     public static void main(String args[])
     {
       int n1=Integer.parseInt(args[0]);
-      int n2=Integer.parseInt(args[1)];
+      int n2=Integer.parseInt(args[1]);
       int r1=n1+n2;
       int r2=n1-n2;
       int r3=n1*n2;
